7d97a99a9b7f5c71aa2f587f254e0568bc89917d
Pedro Augusto <p.augusto0210@gmail.com>
2020-09-07 03:36:53 -0300

    publish website

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,7 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333)
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Umbler listening on port %s', port);
+})
